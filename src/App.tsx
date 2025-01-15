dab7c6105c03ba34e2e7c374def7cedde822049f
Jinwoosung <68122085+Sovereign-ShadowMonarch@users.noreply.github.com>
2025-01-15 17:56:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 
-const API_URL = 'using-lb.gl.at.ply.gg:55681';
+const API_URL = 'https://kind-worlds-argue.loca.lt';
 
 const parseDate = (dateString: string): Date | null => {
   const [day, month, year] = dateString.split('-');
